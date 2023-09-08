d49c2002967ba266efeeb6e72ab07ea12151f31c
Shoeb Mohiuddin <shoebm891@gmail.com>
2023-09-08 18:26:23 +0530

    Authentication complete

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { jwtVerify } from "jose";
+
+export function getJwtSecretKey() {
+  const secret = process.env.TOKEN_SECRET;
+
+  if (!secret) {
+    throw new Error("JWT Secret key is not matched");
+  }
+
+  return new TextEncoder().encode(secret);
+}
+
+export async function verifyJwtToken(token: string | Uint8Array) {
+  try {
+    const { payload } = await jwtVerify(token, getJwtSecretKey());
+
+    // Access the "role" claim from the payload
+    const role = payload.role;
+
+    return role;
+  } catch (error) {
+    return null;
+  }
+}
