a5d33cb872075f48cdfdcbc7321f6c84ced4e82b
Shoeb Mohiuddin <shoebm891@gmail.com>
2023-09-22 17:26:28 +0530

    get user

--- src/app/api/getUser/route.ts
+++ src/app/api/getUser/route.ts
@@ -1,23 +1,23 @@
-import jwt from "jsonwebtoken"; // Import the jwt library
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/db";
-import { verifyJwtToken } from "@/lib/auth";
 import { getUser } from "@/helpers/getUser";
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-export async function GET(request: NextRequest, response: NextResponse) {
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
   try {
-    const user = await getUser(request);
-
-    console.log(user?.id);
-
-    const userFromDb = await prisma.user.findUnique({
-      where: {
-        id: user?.id,
-      },
+    const userId = await getUser(request);
+    const user = await prisma.user.findUnique({
+      where: { id: userId }
+      
+    });
+    console.log(user.name)
+    return NextResponse.json({
+      message: "User found",
+      data: user,
     });
 
-    return NextResponse.json(userFromDb, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
--- src/helpers/getUser.ts
+++ src/helpers/getUser.ts
@@ -1,13 +1,13 @@
+import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
-import { NextRequest, NextResponse } from "next/server";
 
 export const getUser = (request: NextRequest) => {
   try {
-    const token = request.cookies.get("token")?.value || "";
+    const token = request.cookies.get("token")?.value || '';
     const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
-    console.log(decodedToken);
     return decodedToken.id;
   } catch (error: any) {
     throw new Error(error.message);
   }
-};
+
+}
