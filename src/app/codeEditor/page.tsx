e1a1aa9234ce61d53f552e1c1e12416b9ad1a68c
iamsadat <sadatullahkhan40@gmail.com>
2023-09-24 02:41:55 +0530

    Config

--- src/app/codeEditor/page.tsx
+++ src/app/codeEditor/page.tsx
@@ -1,12 +1,167 @@
-import React from "react";
-import CodeEditorWindow from "@/components/MonacoEditor";
+"use client";
 
-const YourComponent = () => {
+import React, { useState } from "react";
+import CodeEditorWindow from "@/components/MonacoEditor";
+import { languageOptions } from "@/lib/languageOptions";
+import { LanguagesDropdown } from "@/components/code/LanguagesDropdown";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+import axios from "axios";
+import { cn } from "@/lib/utils";
+
+const CodeEditor = () => {
+  const [code, setCode] = useState("// Write your code here...");
+  const [customInput, setCustomInput] = useState("");
+  const [outputDetails, setOutputDetails] = useState(null);
+  const [processing, setProcessing] = useState(null);
+  const [theme, setTheme] = useState("cobalt");
+  const [language, setLanguage] = useState(languageOptions[0]);
+
+  const onSelectChange = (sl) => {
+    console.log("selected Option...", sl);
+    setLanguage(sl);
+  };
+
+  const onChange = (action, data) => {
+    switch (action) {
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("case not handled!", action, data);
+      }
+    }
+  };
+
+  const setDarkTheme = (e) => {
+    e.preventDefault();
+    setTheme((prev) => (prev === "vs-dark" ? "hc-black" : "vs-dark"));
+  };
+
+  const setLightTheme = (e) => {
+    e.preventDefault();
+    setTheme("vs-light");
+  };
+
+  const handleCompile = async () => {
+    setProcessing(true);
+    const formData = {
+      language_id: language.id,
+      // encode source code in base64
+      source_code: btoa(code),
+      stdin: btoa(customInput),
+    };
+    const options = {
+      method: "POST",
+      url: process.env.REACT_APP_RAPID_API_URL,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+      },
+      data: formData,
+    };
+
+    console.log(formData);
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log("res.data", response.data);
+        const token = response.data.token;
+        checkStatus(token);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        // get error status
+        let status = err.response?.status;
+        console.log("status", status);
+        if (status === 429) {
+          console.log("too many requests", status);
+        }
+        setProcessing(false);
+        console.log("catch block...", error);
+      });
+  };
+
+  const checkStatus = async (token) => {
+    const options = {
+      method: "GET",
+      url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+      },
+    };
+    try {
+      let response = await axios.request(options);
+      let statusId = response.data.status?.id;
+
+      // Processed - we have a result
+      if (statusId === 1 || statusId === 2) {
+        // still processing
+        setTimeout(() => {
+          checkStatus(token);
+        }, 2000);
+        return;
+      } else {
+        setProcessing(false);
+        setOutputDetails(response.data);
+        console.log("response.data", response.data);
+        return;
+      }
+    } catch (err) {
+      console.log("err", err);
+      setProcessing(false);
+    }
+  };
+
   return (
     <div>
-      <CodeEditorWindow />
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={setLightTheme}>Light</DropdownMenuItem>
+            <DropdownMenuItem onClick={setDarkTheme}>Dark</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <LanguagesDropdown onSelectChange={onSelectChange} />
+      </div>
+      <CodeEditorWindow
+        code={code}
+        onChange={onChange}
+        language={language?.name}
+        theme={theme}
+      />
+      <Button
+        onClick={handleCompile}
+        disabled={!code}
+        className={cn(
+          "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+          !code ? "opacity-50" : ""
+        )}
+      >
+        {processing ? "Processing..." : "Compile and Execute"}
+      </Button>
     </div>
   );
 };
 
-export default YourComponent;
+export default CodeEditor;
--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,10 +1,10 @@
 // CodeEditorWindow.js
-"use client"
+"use client";
 import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
 
-const CodeEditorWindow = ({ onChange, language, code, theme }) => {
+const CodeEditorWindow = ({ onChange, code, language, theme }) => {
   const [value, setValue] = useState(code || "");
 
   const handleEditorChange = (value) => {
@@ -13,26 +13,34 @@ const CodeEditorWindow = ({ onChange, language, code, theme }) => {
   };
 
   return (
-    <div style={{ display: 'flex' }}>
-  <div style={{ flex: '1' }}>
-    {/* Content on the left side */}
-  </div>
-  <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl" style={{ flex: '1', position: 'relative' }}>
-    <div
-      className="resize-handle"
-      style={{ position: 'absolute', top: 0, right: 0, bottom: 0, width: '10px', cursor: 'ew-resize' }}
-    />
-    <Editor
-      height="85vh"
-      width={`100%`}
-      language={language || "javascript"}
-      value={value}
-      theme={theme}
-      defaultValue="// some comment"
-      onChange={handleEditorChange}
-    />
-  </div>
-</div>
+    <div className="flex">
+      <div style={{ flex: "1" }}>{/* Content on the left side */}</div>
+      <div
+        className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl"
+        style={{ flex: "1", position: "relative" }}
+      >
+        <div
+          className="resize-handle"
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            width: "10px",
+            cursor: "ew-resize",
+          }}
+        />
+        <Editor
+          height="85vh"
+          width={`100%`}
+          language={language || "javascript"}
+          value={value}
+          theme={theme}
+          defaultValue="// some comment"
+          onChange={handleEditorChange}
+        />
+      </div>
+    </div>
   );
 };
 export default CodeEditorWindow;
