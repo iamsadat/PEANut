e3968d4ac8a7f788f33259ef143fa4cf3ca7333c
Shoeb Mohiuddin <shoebm891@gmail.com>
2024-01-11 02:17:08 +0530

    added svgs

--- src/app/learn/[id]/page.tsx
+++ src/app/learn/[id]/page.tsx
@@ -23,27 +23,55 @@ export default function Content() {
     }, []);
 
     return (
-        <div className="mx-auto max-w-3xl px-4">
-            {topicsData[path] ? (
-                <div>
-                    <h2 className="text-center text-3xl font-bold underline mt-5 mb-5">{topicsData[path].topic}</h2>
-                    <p className=" text-xl mt-4 mb-8">{topicsData[path].description}</p>
-                    <div className="mt-4">
-                        <h3 className=" text-2xl font-semibold underline mb-10">Formulas</h3>
-                        <ul className="list-disc list-inside text-lg">
-                            {topicsData[path].formulas.map((formula, index) => (
-                                <li key={index}>{formula}</li>
-                            ))}
-                        </ul>
+        <div>
+            <div>
+                <div
+                    className="banner pt-4 sm:pt-navbar px-16 relative flex flex-col justify-center items-center sm:px-0 sm:justify-between sm:flex-row"
+                >
+                    <div className="hidden sm:flex w-88 h-full items-center justify-center">
+                        <img className="w-full pb-20" src="/home1.svg" />
                     </div>
-                    <div className="mt-10">
-                        <h3 className=" text-2xl font-semibold underline">Take a mock test on {topicsData[path].topic}</h3>
-                        <FilteredQuizzes topic={topicsData[path].topic} />
+
+                    <div className="mx-auto max-w-4xl px-3">
+                        {topicsData[path] ? (
+                            <div>
+                                <h2 className="text-center text-3xl font-bold underline mt-2 mb-4">{topicsData[path].topic}</h2>
+                                <p className=" text-xl mt-4 mb-8">{topicsData[path].description}</p>
+                                <div className="mt-4">
+                                    <h3 className=" text-2xl font-semibold underline mb-10">Formulas</h3>
+                                    <ul className="list-disc list-inside text-lg">
+                                        {topicsData[path].formulas.map((formula, index) => (
+                                            <li key={index}>{formula}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <div className="mt-10">
+                                    <h3 className=" text-2xl font-semibold underline mb-5">Take a mock test on {topicsData[path].topic}</h3>
+                                    <FilteredQuizzes topic={topicsData[path].topic} />
+                                </div>
+                            </div>
+                        ) : (
+                            <p className="text-center text-xl mt-10">Loading...</p>
+                        )}
+                    </div>
+                    <div
+                        className=" sm:w-88 flex flex-row-reverse justify-between sm:h-full sm:items-center sm:justify-center"
+                    >
+                        <div>
+                            <img
+                                className="w-60 sm:w-88 pt-8 sm:pt-20"
+                                src="/home2.svg"
+                            />
+                        </div>
+                        <div>
+                            <img
+                                className="w-60 sm:hidden sm:w-full pt-8"
+                                src="/home4.svg"
+                            />
+                        </div>
                     </div>
                 </div>
-            ) : (
-                <p className="text-center text-xl mt-10">Loading...</p>
-            )}
+            </div>
         </div>
     )
 }
