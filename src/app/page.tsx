a013bfc98dde92e0364edaf430bd53a7d191c377
Shoeb Mohiuddin <shoebm891@gmail.com>
2024-01-12 23:29:45 +0530

    Width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default async function Home() {
         className="banner pt-4 sm:pt-navbar px-16 relative flex flex-col justify-center items-center sm:px-0 sm:justify-between sm:flex-row"
       >
         <div className="hidden sm:flex w-88 h-full items-center justify-center">
-          <img className="w-full pb-0" src="/home01.jpg" />
+          <img className="h-auto max-w-md pb-0" src="/home01.jpg" />
         </div>
         <div
-          className="sm:h-full sm:flex sm:flex-1 flex-col items-center justify-center ml-2"
+          className="sm:h-full sm:flex sm:flex-1 flex-col items-center justify-center ml-2 px-5"
         >
           <h1
             className="text-4.5xl sm:text-5xl pt-navbar text-center font-extrabold leading-170"
@@ -28,7 +28,7 @@ export default async function Home() {
             THINK. SOLVE. LEARN.
           </h1>
           <p
-            className="text-1.5xl sm:text-xl font-medium text-center pt-14 sm:w-5/7 leading-160 opacity-60"
+            className="text-1.5xl sm:text-xl font-medium text-center pt-14 sm:w-5/7 leading-160 opacity-60 "
           >
             Peanut is an innovative online platform designed to cater to
             the educational needs of students of LIET,
@@ -42,18 +42,20 @@ export default async function Home() {
         </div>
       </div>
       <div className="banner-footer flex justify-center items-start">
-<Link href={"/login"}
- className="inline-flex flex-col items-center no-underline text-base">
 
-        <img
-          className="pt-0 -mt-14 sm:-mt-0 block sm:h-10"
-          src="/down-arrow.svg"
-        />
-        <span className="block text-grey pt-4 font-semibold text-lg"
-        >Get Started
-        </span>
-</Link>
+        <Link href="/dashboard#settings"
+          className="inline-flex flex-col items-center no-underline text-base">
+
+          <img
+            className="pt-0 -mt-14 sm:-mt-0 block sm:h-10"
+            src="/down-arrow.svg"
+          />
+          <span className="text-1.5xl sm:text-xl font-medium text-center pt-5 sm:w-5/7 leading-160 opacity-60 "
+          >Get Started
+          </span>
+        </Link>
+        
       </div>
-    </div>
+    </div >
   );
 }
